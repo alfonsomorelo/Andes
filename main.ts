8b0fdab934059bda394cc78795f0d5533d6e5359
alfonsomorelo <161080498+alfonsomorelo@users.noreply.github.com>
2024-02-23 15:45:39 -0500

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,5 +1,15 @@
+input.onButtonPressed(Button.A, function () {
+    mbit_Robot.CarCtrlSpeed(mbit_Robot.CarState.Car_Run, 21)
+    if (mbit_Robot.Avoid_Sensor(mbit_Robot.enAvoidState.OBSTACLE)) {
+        mbit_Robot.CarCtrl(mbit_Robot.CarState.Car_Back)
+        basic.pause(1000)
+        mbit_Robot.CarCtrlSpeed(mbit_Robot.CarState.Car_SpinRight, 0)
+        basic.pause(1000)
+    } else {
+        mbit_Robot.CarCtrl(mbit_Robot.CarState.Car_Run)
+    }
+})
 basic.forever(function () {
-    music.play(music.stringPlayable("E D G F B A C5 B ", 120), music.PlaybackMode.UntilDone)
     basic.showLeds(`
         # # # # #
         # . . . #
